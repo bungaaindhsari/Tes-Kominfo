71dcb2745914df204c30576a15deacc3fbd2ccd6
Your Name <you@example.com>
2022-04-08 11:53:57 +0800

    push

---
+++ Table structure admin.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `user` (
+  `username` varchar(20),
+  `password` varchar(20),
+  PRIMARY KEY  (`username`)
+)
+INSERT INTO `user` VALUES ('user1', 'password1');
---
+++ login kominfo.php
@@ -0,0 +1,28 @@
+<!Doctype HTML>
+<html>
+    <head>
+       <title>Halaman Login</title>
+       <link rel="styleshet" type="text/css" href="assets/css/style.css">
+    </head>
+    <body>
+    <h1>Login</h1>
+    <form action="login.php" method="post">		
+		<table>
+			<tr>
+				<td>Username</td>
+				<td><input type="text" name="username"></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td><input type="password" name="password"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" name="login" value="Log In"></td>
+			</tr>
+		</table>
+	</form>
+    </body>
+    
+</html>
+    </head>
